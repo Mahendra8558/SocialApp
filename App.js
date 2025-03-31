153ce15b2b49726d79a413403e4d641a2a74a178
Mahendra8558 <bonganemahendra@gmail.com>
2025-03-31 12:19:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './Pages/Home/Home'
+import Profiles from './Components/Profile/Profiles'
+import Login from './Components/Login/Login'
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
+
+export default App
---
+++ dummydata.js
@@ -0,0 +1,77 @@
+export const Users = [
+    {
+        id:1,
+        profilePic:"./assets/Persons/Kalyankar.jpg",
+        username: "Rahul kalyankar"
+    },
+
+    {
+        id:2,
+        profilePic:"./assets/Persons/Kishor.jpg",
+        username: "Kishor Parekar"
+    },
+
+    {
+        id:3,
+        profilePic:"./assets/Persons/Sunil.jpg",
+        username: "Sunil Gaikwad"
+    },
+    {
+        id:3,
+        profilePic:"./assets/Persons/Vishal.jpg",
+        username: "Vishal Suradkar"
+    },
+    {
+        id:3,
+        profilePic:"./assets/Persons/Yogesh.jpg",
+        username: "Yogesh Borade"
+    }
+];
+
+export const Posts = [
+    {
+        id:1,
+        desc : "Tamhini Ghat",
+        photo : "./assets/Posts/Tamhini2.jpg",
+        date : "5 mins ago",
+        userId : 1,
+        like : 32,
+        comments :  "9 Comments"
+    },
+    {
+        id:2,
+        desc : "Nature's Beauty",
+        photo : "./assets/Posts/Tamhini1.jpg",
+        date : "2 hrs ago",
+        userId : 2,
+        like : 45,
+        comments : "11 Comments"
+    },
+    {
+        id:3,
+        desc : "Enjoy!",
+        photo : "./assets/Posts/Tamhini3.jpg",
+        date : "5 hrs ago",
+        userId : 3,
+        like : 54,
+        comments :  "15 Comments"
+    },
+    {
+        id:4,
+        desc : "Its your life, enjoy it!",
+        photo : "./assets/Posts/Tamhini4.jpg",
+        date : "1 day ago",
+        userId : 4,
+        like : 60,
+        comments : "18 Comments"
+    },
+    {
+        id:5,
+        desc : "Views!",
+        photo : "./assets/Posts/Tamhini5.jpg",
+        date : "2 day ago",
+        userId : 5,
+        like : 75,
+        comments :  "20 Comments"
+    },
+]
